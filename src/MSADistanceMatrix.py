9c19db36e2f7794199f3d6a3c4766cbd5843c767
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-10-25 22:57:39 -0700

    implement script to find distance matrix for an MSA

--- src/MSADistanceMatrix.py
+++ src/MSADistanceMatrix.py
@@ -1,6 +1,78 @@
+import numpy as np
+
+class Sequence:
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+
 class MSADistanceMatrix:
     def __init__(self):
         pass
 
-    def calculate_distance_matrix(self, path):
-        return
+    def _strip_newline(self, string):
+        if string[len(string) - 1] == "\n":
+            return string[0:len(string) - 1]
+        else:
+            return string
+
+    def _parse_file(self, file):
+        sequences = []
+        buf = [None, None]
+        line = file.readline()
+        while line:
+            line = self._strip_newline(line)
+            if(line[0] == '>'):
+                if(buf[0] != None):
+                    sequence = Sequence(buf[0], buf[1])
+                    sequences.append(sequence)
+                name = line[1:]
+                buf[0] = name
+                buf[1] = ''
+            else:
+                buf[1] += line
+            line = file.readline()
+        sequence = Sequence(buf[0], buf[1])
+        sequences.append(sequence)
+        return sequences
+
+    def _get_header_row(self, names):
+        string = 'Sequence'
+        for sequence in names:
+            string += "," + sequence.name
+        string += '\n'
+        return string
+
+    def _parse_to_string(self, names, matrix):
+        string = self._get_header_row(names)
+        for i in range(len(names)):
+            row = names[i].name
+            for j in range(len(names)):
+                row += ',' + str(int(matrix[i][j]))
+            row += '\n'
+            string += row
+        return string
+
+    def _count_num_differences(self, string1, string2):
+        differences = 0
+        length = min(len(string1), len(string2))
+        for i in range(length):
+            if string1[i] != string2[i]:
+                differences += 1
+        return differences + (max(len(string1), len(string2)) - length)
+
+    def _generate_matrix(self, sequences):
+        dim = len(sequences)
+        matrix = np.zeros((dim, dim))
+        for i in range(dim):
+            row_sequence = sequences[i].sequence
+            for j in range(len(sequences)):
+                column_sequence = sequences[j].sequence
+                matrix[i][j] = self._count_num_differences(row_sequence, column_sequence)
+        return matrix
+
+    def get_distance_matrix_string(self, path):
+        file = open(path, "r")
+        sequences = self._parse_file(file)
+        matrix = self._generate_matrix(sequences)
+        string = self._parse_to_string(sequences, matrix)
+        return string
--- test/test_MSADistanceMatrix.py
+++ test/test_MSADistanceMatrix.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
-
+from MSADistanceMatrix import MSADistanceMatrix
 
 class TestMSADistanceMatrix(TestCase):
-    def test_calculate_distance_matrix(self):
-        self.fail()
+    def setUp(self):
+        self.matrix = MSADistanceMatrix()
+
+    def test_get_distance_matrix_string(self):
+        csv_matrix = self.matrix.get_distance_matrix_string('data/all_variants_trimal.mfa')
+        self.assertEqual(csv_matrix, "Sequence,ref,Bat,Cat\nref,0,6,26\nBat,6,0,22\nCat,26,22,0\n")
